a193c0540814e67e53092d74c5c27c08f4109bcd
Katie Moore <katiemoore@katies-mbp.mynet>
2021-04-25 15:43:55 +0100

    JS comments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// Whilst this JS works you could achieve the same effect in CSS like this
+// .card:hover {
+//     border-color: mediumSeaGreen;
+// }
+
+// In general, if something can be achieved with CSS instead of JS then you should opt to do in CSS
+// because this is better for perfomance and also some people might have their JS disabled in the browser
+// and so won't have access to that funcionality. 
+
 function changeBg(obj, colorName) {
     obj.style.borderColor = colorName;
 }
+
+// For this code test you could play around with creating your own carousel for the images, here is an example of how to 
+// do something like that: https://www.w3schools.com/howto/howto_js_slideshow.asp
